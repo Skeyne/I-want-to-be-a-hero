627b5e48c3964008627adf60a6b8d9211ea8bff7
Skeyne <andremrodrigues99@gmail.com>
2022-12-29 19:10:59 +0000

    v0.03d

--- training.js
+++ training.js
@@ -97,7 +97,7 @@ class Activity {
                 if((rewards[index] > 0)){
                     expReward += Math.log10(playerStats[attribute]+1) * this.timeToComplete/10000;
                 }
-                playerStats[attribute] = Math.max(0,playerStats[attribute]+reward);
+                playerStats[attribute] = Math.max(playerStats.permanentAttributes[index],playerStats[attribute]+reward);
             }
         }
         playerStats.activityLevels[this.id].exp += expReward;
