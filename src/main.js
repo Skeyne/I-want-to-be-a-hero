66c516844e867de521a4f2ccb600387ac270e12e
Skeyne <andremrodrigues99@gmail.com>
2023-01-19 11:47:22 +0000

    v0.05d

--- src/main.js
+++ src/main.js
@@ -223,6 +223,24 @@ document.addEventListener('DOMContentLoaded', (e) => {
     setTimeout(() => { document.getElementById("splashScreen").className += ' splashScreenFadeOut' }, 1000)
     setTimeout(() => { document.getElementById("splashScreen").style.display = 'none' }, 2000)
 });
+//unintended scrolling
+var keys = {};
+window.addEventListener("keydown",
+    function(e){
+        keys[e.code] = true;
+        switch(e.code){
+            case "ArrowUp": case "ArrowDown": case "ArrowLeft": case "ArrowRight":
+            case "Space": e.preventDefault(); break;
+            default: break; // do not block other keys
+        }
+    },
+false);
+window.addEventListener('keyup',
+    function(e){
+        keys[e.code] = false;
+    },
+false);
+
 // let isMouseHover = false
 // leftWindow.addEventListener("mouseleave", function (event) {
 //     pageY = 0;
